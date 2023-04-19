7b464de467f1f6947b49eb35768a52011a194217
Vitalii1991 <ironwarrior1991@gmail.com>
2023-04-19 21:51:24 +0300

    1

---
+++ js/task-04.js
@@ -0,0 +1,25 @@
+const counterValue = {
+  value: 0,
+  decrement(value) {
+    this.value -= 1;
+  },
+  increment(value) {
+    this.value += 1;
+  },
+};
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector("#value");
+
+decrementBtn.addEventListener("click", function () {
+  counterValue.decrement();
+
+  valueEl.textContent = counterValue.value;
+});
+
+incrementBtn.addEventListener("click", function () {
+  counterValue.increment();
+
+  valueEl.textContent = counterValue.value;
+});
