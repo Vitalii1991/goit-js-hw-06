6c7b0a1173641c30f384ac1aa254f39d8ed47102
Vitalii1991 <ironwarrior1991@gmail.com>
2023-04-19 20:10:45 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+  const ingredientsItem = document.createElement("li");
+  ingredientsItem.classList.add("item");
+  ingredientsItem.textContent = ingredient;
+
+  ingredientsList.append(ingredientsItem);
+}
+
+console.log(ingredientsList);
