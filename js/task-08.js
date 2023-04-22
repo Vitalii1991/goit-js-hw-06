ae037a91125f75e322ec91eddc1331d8df31432e
Vitalii1991 <ironwarrior1991@gmail.com>
2023-04-22 16:06:49 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector(".login-form");
+const inputEmailRef = document.querySelector('input[type="email"]');
+const inputPasswordRef = document.querySelector('input[type="password"]');
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (inputEmailRef.value === "" || inputPasswordRef.value === "") {
+    alert("Все поля должны быть заполнены!");
+  }
+
+  const elements = event.currentTarget;
+
+  const valueForm = {
+    email: elements.email.value,
+    password: elements.password.value,
+  };
+  console.log(valueForm);
+
+  elements.reset();
+}
