a81dc571244e36a16a7bfebba4823844155444ab
Vitalii1991 <ironwarrior1991@gmail.com>
2023-04-23 18:42:32 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -12,13 +12,13 @@ const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 const valueEl = document.querySelector("#value");
 
-decrementBtn.addEventListener("click", function () {
+decrementBtn.addEventListener("click", () => {
   counterValue.decrement();
 
   valueEl.textContent = counterValue.value;
 });
 
-incrementBtn.addEventListener("click", function () {
+incrementBtn.addEventListener("click", () => {
   counterValue.increment();
 
   valueEl.textContent = counterValue.value;
