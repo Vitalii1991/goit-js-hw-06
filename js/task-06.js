5a7553ee3a5956af7649ec538d639bee3e5fcec1
Vitalii1991 <ironwarrior1991@gmail.com>
2023-04-25 11:36:45 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,11 @@
 const inputRef = document.querySelector("#validation-input");
-const inputDataLength = inputRef.getAttribute("data-length");
-const inputDataLengthNumber = parseInt(inputDataLength);
 
-inputRef.addEventListener("input", () => {});
-inputRef.addEventListener("blur", onInputBlur);
-
-function onInputBlur(event) {
-  if (event.currentTarget.value.length >= inputDataLengthNumber) {
+inputRef.addEventListener("blur", (event) => {
+  if (Number(inputRef.dataset.length) === event.currentTarget.value.length) {
     inputRef.classList.remove("invalid");
     inputRef.classList.add("valid");
   } else {
     inputRef.classList.remove("valid");
     inputRef.classList.add("invalid");
   }
-}
+});
