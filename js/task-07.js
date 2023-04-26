a857de69c5fcaa649d5de3e392ce94b0c11f221b
Vitalii1991 <ironwarrior1991@gmail.com>
2023-04-26 14:50:06 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const inputRef = document.querySelector("#font-size-control");
 const spanTextRef = document.querySelector("#text");
 
-spanTextRef.style.fontSize = spanTextRef.value + "px";
+spanTextRef.style.fontSize = inputRef.value + "px";
 
 inputRef.addEventListener("input", (event) => {
   spanTextRef.style.fontSize = event.currentTarget.value + "px";
